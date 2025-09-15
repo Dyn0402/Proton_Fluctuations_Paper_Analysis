1a4986613dda93c062f26bd9276dba91dbe83d22
Dylan <Dyn0402@hotmail.com>
2025-09-15 20:42:01 +0200

    calc_binom_slices.py, istarmap.py put everything back
    Think I can use my python 3.9 on rcf

--- Python_Downstream/calc_binom_slices.py
+++ Python_Downstream/calc_binom_slices.py
@@ -18,8 +18,7 @@ from time import sleep
 
 from multiprocessing import Pool
 import tqdm
-# import istarmap  # Needed for tqdm
-from istarmap import starmap_with_progress
+import istarmap  # Needed for tqdm
 
 from BootstrapAzBin import BootstrapAzBin
 from Measure import Measure
@@ -235,17 +234,13 @@ def read_data(pars):
         jobs.extend(get_dataset_jobs(dataset, pars))
     if pars['check_only']:  # Just check the files and return
         with Pool(pars['threads']) as pool:
-            # for df_subset in tqdm.tqdm(pool.istarmap(check_subset, jobs), total=len(jobs)):
-            #     pass
-            for _ in starmap_with_progress(pool, check_subset, jobs, total=len(jobs)):
+            for df_subset in tqdm.tqdm(pool.istarmap(check_subset, jobs), total=len(jobs)):
                 pass
         return
 
     df_subsets = []
     with Pool(pars['threads']) as pool:
-        # for df_subset in tqdm.tqdm(pool.istarmap(read_subset, jobs), total=len(jobs)):
-        #     df_subsets.extend(df_subset)
-        for df_subset in starmap_with_progress(pool, read_subset, jobs, total=len(jobs)):
+        for df_subset in tqdm.tqdm(pool.istarmap(read_subset, jobs), total=len(jobs)):
             df_subsets.extend(df_subset)
 
     df = pd.DataFrame(df_subsets)
@@ -617,9 +612,7 @@ def get_systematics(pars, df):
 
     jobs = [(df[df['name'] == sys_set['default']['name']], sys_set) for sys_set in pars['sys_sets']]
     with Pool(pars['threads']) as pool:
-        # for sys_set_list in tqdm.tqdm(pool.istarmap(get_sys_set, jobs), total=len(jobs)):
-        #     sys_df.extend(sys_set_list)
-        for sys_set_list in starmap_with_progress(pool, get_sys_set, jobs, total=len(jobs)):
+        for sys_set_list in tqdm.tqdm(pool.istarmap(get_sys_set, jobs), total=len(jobs)):
             sys_df.extend(sys_set_list)
 
     return pd.DataFrame(sys_df)
--- Python_Downstream/istarmap.py
+++ Python_Downstream/istarmap.py
@@ -8,42 +8,29 @@ Created as QGP_Scripts/istarmap
 @author: Dylan Neff, dylan
 """
 
-from multiprocessing import Pool
-from tqdm import tqdm
-
-def starmap_with_progress(pool, func, iterable, total=None, chunksize=1):
-    """
-    Like starmap but iterable with tqdm progress.
-    Works on Python 3.7+ without messing with Pool internals.
-    """
-    # Wrap args with a lambda that unpacks them
-    return tqdm(pool.imap(lambda args: func(*args), iterable, chunksize),
-                total=total)
-
-
 # istarmap.py for Python 3.8+
-# import multiprocessing.pool as mpp
-#
-#
-# def istarmap(self, func, iterable, chunksize=1):
-#     """starmap-version of imap
-#     """
-#     self._check_running()
-#     if chunksize < 1:
-#         raise ValueError(
-#             "Chunksize must be 1+, not {0:n}".format(
-#                 chunksize))
-#
-#     task_batches = mpp.Pool._get_tasks(func, iterable, chunksize)
-#     result = mpp.IMapIterator(self)
-#     self._taskqueue.put(
-#         (
-#             self._guarded_task_generation(result._job,
-#                                           mpp.starmapstar,
-#                                           task_batches),
-#             result._set_length
-#         ))
-#     return (item for chunk in result for item in chunk)
-#
-#
-# mpp.Pool.istarmap = istarmap
+import multiprocessing.pool as mpp
+
+
+def istarmap(self, func, iterable, chunksize=1):
+    """starmap-version of imap
+    """
+    self._check_running()
+    if chunksize < 1:
+        raise ValueError(
+            "Chunksize must be 1+, not {0:n}".format(
+                chunksize))
+
+    task_batches = mpp.Pool._get_tasks(func, iterable, chunksize)
+    result = mpp.IMapIterator(self)
+    self._taskqueue.put(
+        (
+            self._guarded_task_generation(result._job,
+                                          mpp.starmapstar,
+                                          task_batches),
+            result._set_length
+        ))
+    return (item for chunk in result for item in chunk)
+
+
+mpp.Pool.istarmap = istarmap
